2ed8dc6f03dd8faad18793e924812d030edbb0f3
MORT11 <programming@mort11.org>
2023-11-21 17:42:15 -0500

    First commit

---
+++ src/main/java/org/mort11/commands/RotateToAngle.java
@@ -0,0 +1,43 @@
+package org.mort11.commands;
+
+import org.mort11.subsystems.Drivetrain;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RotateToAngle extends CommandBase {
+	private Drivetrain drivetrain;
+	private double angle;
+	private boolean relative;
+
+	public RotateToAngle(double angle, boolean relative) {
+		drivetrain = Drivetrain.getInstance();
+		this.angle = angle;
+		this.relative = relative;
+
+		addRequirements(drivetrain);
+	}
+
+	@Override
+	public void initialize() {
+		if (relative) {
+			angle += drivetrain.getGyroscopeRotation().getDegrees();
+		}
+	}
+
+	@Override
+	public void execute() {
+		drivetrain.drive(new ChassisSpeeds(0, 0, drivetrain.getRotateToAngleController()
+				.calculate(drivetrain.getGyroscopeRotation().getDegrees(), angle)));
+	}
+
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		drivetrain.drive(new ChassisSpeeds(0, 0, 0));
+	}
+}
